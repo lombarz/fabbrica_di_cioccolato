605eb8445c9f60785dfd62d454c7e2bf1d44d5ef
lombarz <137717516+lombarz@users.noreply.github.com>
2024-09-27 15:59:55 +0200

    cioccolatino
    27/09 16:00

---
+++ cioccolatino.py
@@ -0,0 +1,8 @@
+class Cioccolatino:
+    def __init__(self,forma,ripieno,numero):
+        self.forma=forma
+        self.ripieno=ripieno
+        self.numero=numero
+        self.qty=2*self.numero
+        
+
